02e44dc73ca96492bc58352ae0dae9af45e9e942
Vikash <vishalvikash8651@gmail.com>
2022-11-01 00:28:22 +0530

    first commit

---
+++ login2.js
@@ -0,0 +1,19 @@
+function validate()
+{
+var username=document.getElementById("username").value;
+var password=document.getElementById("password").value;
+if(username=="admin"&& password=="user")
+{
+    
+
+    
+    location.href = "https://cgu-odisha.ac.in/placement-record-2018-2022-batch/"
+    
+}
+else
+{
+    alert("login failed");
+}
+
+
+}
